67d5bdcfa78628e3231ca8341572647acfea1003
Rylael <daniel.kovesdi@gmail.com>
2025-02-17 11:15:34 +0100

    added wordcount

--- main.py
+++ main.py
@@ -5,4 +5,10 @@ def main():
     with open('./books/frankenstein.txt') as f:
         file_contents = f.read()
     print(file_contents)
+    print(len(file_contents.split()))
+
+#def wordcount():
+    #with open('./books/frankenstein.txt') as f:
+     #   file_contents = f.read()
+    #print(len(file_contents.split()))
 main()
