40bed843e266d5d303ca1e0984159fbe4ed2cf3a
Rylael <daniel.kovesdi@gmail.com>
2025-02-18 12:25:01 +0100

    added report

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    characters = char_count(text)
+    #num_words = get_num_words(text)
+    #characters = char_count(text)
+    report = count_and_sort_chars(text)
     print(f"{num_words} words found in the document")
 
 
@@ -26,4 +27,16 @@ def char_count(text):
     print("Final character count:", char_dict)
     return char_dict
     
+    
+def count_and_sort_chars(text):
+    char_dict = {}
+    text = text.lower()
+    for char in text:
+        if char.isalpha():
+            if char in char_dict:
+                char_dict[char] += 1
+            else:
+                char_dict[char] = 1
+    for key in sorted(char_dict, key=char_dict.get, reverse=True):
+        print(f"'{key}': {char_dict[key]}")
 main()
